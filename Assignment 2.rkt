1557bcc5951296c03e79939437b7526abe6303be
MarinaNikiforova <35703824+MarinaNikiforova@users.noreply.github.com>
2018-11-07 17:36:08 -0800

    Update Assignment 2.rkt
    Corrected function 3

--- Assignment 2.rkt
+++ Assignment 2.rkt
@@ -6,13 +6,22 @@
 |#
 
 #|-----Function3------|#
- (define(buildList N E1)
+ (define(buildOne N E1)
     (if (= N 0) '()
-      (cons E1(buildList (- N 1) E1))))
+      (cons E1(buildOne (- N 1) E1))))
+(define(buildTwo M E2)
+    (if (= M 0) '()
+      (cons E2(buildTwo (- M 1) E2))))
+(define(appendlist E1 E2)(if (null? E1) E2
+                             (cons(car E1)
+                                  (appendlist(cdr E1) E2))))
+(define (buildList N E1 M E2)
+  (appendlist (buildOne N E1) (buildTwo M E2)))
   
-     
-;testing (works but needs to be extended)
-(buildList 3 'A)
+  ;test
+(buildList 3 'A 3 'b)
+(buildList 3 '() 2 'a)
+(buildList 2 '(a b c) 1 'Q)
 
 #|-----Function 6-----|#
 (define (selectN n)
